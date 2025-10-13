64ee51c43826146885fb60ced20336561e43b5c4
Ernest Litvin <erik.litvin@gmail.com>
2025-10-13 10:03:03 +0300

    Cleaning data

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ df = pd.read_csv("matches_1930_2022.csv")
 
 # df.info()
 
+### Preparing columns for data analysis : ###
+## 1.1 Making small letters: ##
 df.columns = [col.lower() for col in df.columns]
 # df.info()
 
-cols = [3,6,8,9,10,11,18,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43]
+### 1.2. Deleting unnecessary columns ###
+cols = [3,4,6,7,8,9,10,11,13,14,18,19,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43]
 df.drop(df.columns[cols], axis=1, inplace=True)
 
-df.info()
+## 1.3. Making "date" as DATE ##
+
 
 
