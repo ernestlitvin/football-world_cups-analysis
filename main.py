31cc5c3470e4b0761ad9b2e55d7f37edcc143086
Ernest Litvin <erik.litvin@gmail.com>
2025-10-13 14:54:14 +0300

    Basic statistics - Medalists

--- main.py
+++ main.py
@@ -29,29 +29,31 @@ df.replace("West Germany", "Germany", inplace=True)
 print(f"\n1st World Cup was in {df.year.min()}.\n The last World Cup was in {df.year.max()}.")
 print(f"\nMatches played on all WC: {dfwc.matches.sum()}")
 
-
+## creating top20 teams, which played most of games on WC
 home_team_played = df["home_team"].value_counts()
 away_team_played = df["away_team"].value_counts()
 team_played = (home_team_played + away_team_played).sort_values(ascending=False).dropna()
 top20_teams = team_played.head(20)
 
-plt.figure(figsize=(12, 7))
-ax = sns.barplot(x=top20_teams.index, y=top20_teams.values, hue = top20_teams.index, palette = "Set2", legend = False)
+## creating visualization
+# plt.figure(figsize=(12, 7))
+# ax = sns.barplot(x=top20_teams.index, y=top20_teams.values, hue = top20_teams.index, palette = "Set2", legend = False)
+#
+# for container in ax.containers:
+#     ax.bar_label(container, fontsize=10)
 
-for container in ax.containers:
-    ax.bar_label(container, fontsize=10)
-
-plt.xticks(rotation=45, ha = "right")
-plt.xlabel("Teams")
-plt.ylabel("Games played on all WC")
-plt.title("Top 20 teams with most played games on WC")
-plt.tight_layout()
-plt.grid(True, axis = "y", linestyle = "--")
+# plt.xticks(rotation=45, ha = "right")
+# plt.xlabel("Teams")
+# plt.ylabel("Games played on all WC")
+# plt.title("Top 20 teams with most played games on WC")
+# plt.tight_layout()
+# plt.grid(True, axis = "y", linestyle = "--")
 # plt.show()
 
-
+## renaming values for better analysis
 dfwc.rename(columns ={"champion": "gold", "runner-up": "silver"}, inplace = True)
 
+## creating the list of "bronze"
 third_place_matches = df[df["round"] == "Third-place match"]
 third_place = np.where(
     third_place_matches["home_score"] > third_place_matches["away_score"],
@@ -73,10 +75,22 @@ world_cup_full = pd.merge(
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
-gold_winner = dfwc["gold"].value_counts()
-silver_winner = dfwc["silver"].value_counts()
-bronze_winner = world_cup_full["bronze"].value_counts()
+gold_winners = world_cup_full["gold"].value_counts().rename("gold")
+silver_winners = world_cup_full["silver"].value_counts().rename("silver")
+bronze_winners = world_cup_full["bronze"].value_counts().rename("bronze")
 
+medalists_df = pd.concat([gold_winners, silver_winners, bronze_winners], axis=1)
+medalists_df.fillna(0, inplace=True)
+print(f"\n{medalists_df}")
+
+## visualization of all medalists
+
+# plt.figure(figsize=(12, 7))
+# sns.countplot(data = world_cup_full, x = "gold")
+# sns.countplot(data = world_cup_full, x = "silver")
+# sns.countplot(data = world_cup_full, x = "bronze")
+# plt.xticks(rotation=45, ha = "right")
+# plt.show()
 
 
 
