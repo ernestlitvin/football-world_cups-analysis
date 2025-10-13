b96a9a5d71dd2750584679eade41c9732a6bbf83
Ernest Litvin <erik.litvin@gmail.com>
2025-10-13 12:06:55 +0300

    Basic statistics

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ dfwc = pd.read_csv("world_cup.csv")
 
 ## 1.1 Making small letters of columns: ##
 df.columns = [col.lower() for col in df.columns]
+dfwc.columns = [col.lower() for col in dfwc.columns]
+
 
 ### 1.2. Deleting unnecessary columns ###
 cols = [3,4,6,7,8,9,10,11,13,14,18,19,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43]
@@ -19,13 +21,27 @@ df["date"] = pd.to_datetime(df["date"])
 ## 1.4 Checking duplicate columns ##
 print(f"Duplicates amount: {df.duplicated().sum()}")
 
+
+
 ## Basic statistics: ##
 print(f"\n1st World Cup was in {df.year.min()}.\n The last World Cup was in {df.year.max()}.")
-print(f"\nMatches played on all WC: {dfwc.Matches.sum()}")
+print(f"\nMatches played on all WC: {dfwc.matches.sum()}")
+
 
 home_team_played = df["home_team"].value_counts()
 away_team_played = df["away_team"].value_counts()
-team_played = home_team_played + away_team_played
-print(f"\nTeam played games on all WC: \n {team_played.sort_values(ascending=False)}")
+team_played = (home_team_played + away_team_played).sort_values(ascending=False).dropna()
+
+plt.figure(figsize=(12, 6))
+sns.barplot(x=team_played.index, y=team_played.values, hue = team_played.index, palette = "Set2", legend = False)
+plt.xticks(rotation=45)
+plt.xlim(0,20)
+plt.xlabel("Teams")
+plt.ylabel("Games played on all WC")
+plt.title("Top 20 teams with most played games on WC")
+plt.tight_layout()
+plt.grid(True)
+plt.show()
+
 
 
